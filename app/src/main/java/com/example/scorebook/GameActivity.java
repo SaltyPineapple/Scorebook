677efdc7ef4ef14d7038b32c33dee46f1a15e89d
Mark Pratt <mjpratt31@gmail.com>
2022-01-05 16:20:58 -0800

    Add new round to game

--- app/src/main/java/com/example/scorebook/GameActivity.java
+++ app/src/main/java/com/example/scorebook/GameActivity.java
@@ -3,14 +3,21 @@ package com.example.scorebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 public class GameActivity extends AppCompatActivity {
 
     public TextView mTitleTextView;
+    private int players;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,26 +28,61 @@ public class GameActivity extends AppCompatActivity {
         mTitleTextView.setText(getIntent().getStringExtra("title"));
 
         int players = getIntent().getIntExtra("players", 0);
+        this.players = players;
 
 
-        init(players);
+        init();
+
+        FloatingActionButton fab = findViewById(R.id.fabNewRound);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newRound();
+                Snackbar.make(v, "Added round!", Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+            }
+        });
 
 
     }
 
-    public void init(int players){
+    public void init(){
         TableLayout scoreTable = (TableLayout) findViewById(R.id.scoreTable);
-        for(int x=0; x<players-1; x++){
+        for(int x=0; x<players; x++){
             TableRow tbrow = new TableRow(this);
+            tbrow.setId(100 + x);
 
-            EditText etRow = new EditText(this);
-            etRow.setText("Player" + x+1);
-            etRow.setTextSize(24);
+            EditText etPlayerRow = new EditText(this);
+            String text = "Player " + (x+1);
+            etPlayerRow.setText(text);
+            etPlayerRow.setTextSize(24);
+            tbrow.addView(etPlayerRow);
 
-            tbrow.addView(etRow);
+            EditText etScoreRow = new EditText(this);
+            etScoreRow.setTextSize(24);
+            etScoreRow.setInputType(InputType.TYPE_CLASS_NUMBER);
+            etScoreRow.setMinWidth(100);
+            tbrow.addView(etScoreRow);
+            etScoreRow.getLayoutParams().width = ViewGroup.LayoutParams.WRAP_CONTENT;
 
             scoreTable.addView(tbrow);
 
+        }
+
+    }
+
+    public void newRound(){
+        TableLayout scoreTable = (TableLayout) findViewById(R.id.scoreTable);
+
+        for(int x=0; x<players; x++){
+            TableRow tbrow = findViewById(100+x);
+            EditText etScoreRow = new EditText(this);
+            etScoreRow.setTextSize(24);
+            etScoreRow.setMinWidth(100);
+            etScoreRow.setInputType(InputType.TYPE_CLASS_NUMBER);
+            tbrow.addView(etScoreRow);
+
+            etScoreRow.getLayoutParams().width = ViewGroup.LayoutParams.WRAP_CONTENT;
+            //scoreTable.addView(etRow);
         }
 
     }
